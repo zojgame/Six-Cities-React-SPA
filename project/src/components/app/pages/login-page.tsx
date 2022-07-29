2da3a6672ec3705fc300aa1bfedccade123a23d2
zo-game <68361665+zo-game@users.noreply.github.com>
2022-07-29 10:01:24 +0800

    get token after autorization

--- project/src/components/app/pages/login-page.tsx
+++ project/src/components/app/pages/login-page.tsx
@@ -1,10 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useNavigate } from 'react-router-dom';
 import Logo from '../logo/logo';
+import { FormEvent, useRef } from 'react';
+import { AppRoute } from '../../const';
+import { useAppDispatch } from '../../../hooks';
+import {loginAction} from '../../../store/api-actions';
+import {AuthData} from '../../../types/auth-data';
+
+// type AuthData = {
+//   login: string,
+//   email: string
+// }
 
 function LoginPage():JSX.Element{
+  // const navigate = useNavigate();
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if(emailRef.current !== null && passwordRef.current !== null){
+      const authData : AuthData = {
+        login: emailRef.current.value,
+        password: passwordRef.current.value
+      };
+
+      onSubmit(authData);
+    }
+  };
   return (
     <body>
       <div style={{display: 'none'}}>
-        <svg xmlns="http://www.w3.org/2000/svg"><symbol id="icon-arrow-select" viewBox="0 0 7 4"><path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path></symbol><symbol id="icon-bookmark" viewBox="0 0 17 18"><path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path></symbol><symbol id="icon-star" viewBox="0 0 13 12"><path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path></symbol></svg>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <symbol id="icon-arrow-select" viewBox="0 0 7 4">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path>
+          </symbol>
+          <symbol id="icon-bookmark" viewBox="0 0 17 18">
+            <path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path>
+          </symbol>
+          <symbol id="icon-star" viewBox="0 0 13 12">
+            <path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path>
+          </symbol>
+        </svg>
       </div>
 
       <div className="page page--gray page--login">
@@ -22,16 +66,31 @@ function LoginPage():JSX.Element{
           <div className="page__login-container container">
             <section className="login">
               <h1 className="login__title">Sign in</h1>
-              <form className="login__form form" action="#" method="post">
+              <form className="login__form form" action="#" method="post"
+                onSubmit={handleSubmit}
+              >
                 <div className="login__input-wrapper form__input-wrapper">
                   <label className="visually-hidden">E-mail</label>
-                  <input className="login__input form__input" type="email" name="email" placeholder="Email" required={false} />
+                  <input
+                    ref={emailRef}
+                    className="login__input form__input"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required={false}
+                  />
                 </div>
                 <div className="login__input-wrapper form__input-wrapper">
                   <label className="visually-hidden">Password</label>
-                  <input className="login__input form__input" type="password" name="password" placeholder="Password" required={false} />
+                  <input className="login__input form__input"
+                    ref={passwordRef}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required={false}
+                  />
                 </div>
-                <button className="login__submit form__submit button" type="submit">Sign in</button>
+                <button className="login__submit form__submit button" type="submit" >Sign in</button>
               </form>
             </section>
             <section className="locations locations--login locations--current">
